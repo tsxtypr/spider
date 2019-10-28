e546db38a673d68ea4c906536203ef385c3c65c5
lkk <1311762691@qq.com>
2019-10-28 20:52:01 +0800

    百度翻译

--- post_request/fanyi_spider.py
+++ post_request/fanyi_spider.py
@@ -1,25 +1,57 @@
+# import requests
+#
+# base_url="https://fanyi.baidu.com/sug"
+#
+# kw="python"
+# data={
+#     "kw":kw
+# }
+#
+# headers={
+#     "content-length":str(len(kw)),
+#     "content-type":"application/x-www-form-urlencoded;",
+#     "origin":"https://fanyi.baidu.com",
+#     "referer":"https://fanyi.baidu.com/",
+#     "user-agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36",
+#     "x-requested-with":"XMLHttpRequest"
+# }
+#
+# response=requests.post(url=base_url,headers=headers,data=data)
+# #post请求返回的数据都是json数据
+# print(response.json()["data"])
+# result=""
+# for data in response.json()["data"]:
+#     result+=data["v"]+"\n"
+# print(result)
+
+
+#面向对象，对请求的数据进行封装
 import requests
 
-base_url="https://fanyi.baidu.com/sug"
-
-kw="python"
-data={
-    "kw":kw
+class Search_words:
+    def __init__(self,words):
+        self.words=words
+        self.base_url="https://fanyi.baidu.com/sug"
+        self.data={
+    "kw":self.words
 }
-
-headers={
-    "content-length":str(len(kw)),
+        self.headers={
+    "content-length":str(len(self.words)),
     "content-type":"application/x-www-form-urlencoded;",
     "origin":"https://fanyi.baidu.com",
     "referer":"https://fanyi.baidu.com/",
     "user-agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36",
     "x-requested-with":"XMLHttpRequest"
 }
+    def get_data(self):
+        response=requests.post(url=self.base_url,headers=self.headers,data=self.data)
+        result=""
+        for data in response.json()["data"]:
+            result+=data["v"]+"\n"
+        return result
+    def __str__(self):
+        return self.get_data()
 
-response=requests.post(url=base_url,headers=headers,data=data)
-#post请求返回的数据都是json数据
-print(response.json()["data"])
-result=""
-for data in response.json()["data"]:
-    result+=data["v"]+"\n"
-print(result)
+if __name__ == '__main__':
+    data=Search_words("python")
+    print(data)
